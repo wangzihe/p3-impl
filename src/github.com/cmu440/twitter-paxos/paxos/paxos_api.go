28926fa629ed03ab3e5efcc88fa8ca638829206a
sss1 <sss1@andrew.cmu.edu>
2014-04-25 13:23:06 -0400

    Switched contents of paxos_api.go and paxos_impl.go

--- src/github.com/cmu440/twitter-paxos/paxos/paxos_api.go
+++ src/github.com/cmu440/twitter-paxos/paxos/paxos_api.go
@@ -1,10 +1,36 @@
-// paxos interface
+// Implementation for paxos
 package paxos
 
-type paxosStates struct {
-}
+type Paxos interface {
+	// This function is used by a node to make a new proposal
+	// to other nodes. It will return true when a proposal is
+	// being successfully made. Otherwise, it will return false.
+	Prepare() (bool, error)
 
-// This function creates a paxos package for storage server
-// to use.
-func NewPaxos() (paxosStates, error) {
+	// This function is used by a leader to do all the work in
+	// accept phase. It will return true when a majority of nodes
+	// reply accept-ok. Otherwise, it will return false.
+	Accept() (bool, error)
+
+	// This function is used by a leader to commit a value after
+	// a successful accept phase.
+	CommitVal() error
+
+	// This function is used by a leader upon receiving a prepare
+	// response from an acceptor. It will react corresponding to
+	// the message and make updates to the current state.
+	ReceivePrepareResponse()
+
+	// This function is used by a leader upon receiving an accept
+	// response from an acceptor. It will react corresponding to
+	// the message and make updates to the current state.
+	ReceiveAcceptResponse()
+
+	// This function is used by an acceptor to react to a prepare
+	// message. It will react corresponding to the current state.
+	ReceivePropose()
+
+	// This function is used by an acceptor to react to an accept
+	// message. It will react corresponding to the current state.
+	ReceiveAccept()
 }
--- src/github.com/cmu440/twitter-paxos/paxos/paxos_impl.go
+++ src/github.com/cmu440/twitter-paxos/paxos/paxos_impl.go
@@ -1,36 +1,10 @@
-// Implementation for paxos
+// paxos interface
 package paxos
 
-type Paxos interface {
-	// This function is used by a node to make a new proposal
-	// to other nodes. It will return true when a proposal is
-	// being successfully made. Otherwise, it will return false.
-	Prepare() (bool, error)
+type paxosStates struct {
+}
 
-	// This function is used by a leader to do all the work in
-	// accept phase. It will return true when a majority of nodes
-	// reply accept-ok. Otherwise, it will return false.
-	Accept() (bool, error)
-
-	// This function is used by a leader to commit a value after
-	// a successful accept phase.
-	CommitVal() error
-
-	// This function is used by a leader upon receiving a prepare
-	// response from an acceptor. It will react corresponding to
-	// the message and make updates to the current state.
-	ReceivePrepareResponse()
-
-	// This function is used by a leader upon receiving an accept
-	// response from an acceptor. It will react corresponding to
-	// the message and make updates to the current state.
-	ReceiveAcceptResponse()
-
-	// This function is used by an acceptor to react to a prepare
-	// message. It will react corresponding to the current state.
-	ReceivePropose()
-
-	// This function is used by an acceptor to react to an accept
-	// message. It will react corresponding to the current state.
-	ReceiveAccept()
+// This function creates a paxos package for storage server
+// to use.
+func NewPaxos() (paxosStates, error) {
 }
