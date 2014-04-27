52d0b91f190137ce40056ca655ffcbda808a73c2
sss1 <sss1@andrew.cmu.edu>
2014-04-27 17:11:07 -0400

    Restructured a lot of paxos.

--- src/github.com/cmu440/twitter-paxos/paxos/paxos_api.go
+++ src/github.com/cmu440/twitter-paxos/paxos/paxos_api.go
@@ -1,32 +1,20 @@
 // paxos interface
 package paxos
 
-type Paxos interface {
+type PaxosStates interface {
 	// This function is used by a node to make a new proposal
 	// to other nodes. It will return true when a proposal is
 	// being successfully made. Otherwise, it will return false.
-	Prepare() (bool, error)
+	Prepare([]string, []byte) (bool, error)
 
 	// This function is used by a leader to do all the work in
 	// accept phase. It will return true when a majority of nodes
 	// reply accept-ok. Otherwise, it will return false.
-	Accept() (bool, error)
+	Accept(string, string) (bool, error)
 
-	// This function is used by a leader upon receiving a prepare
-	// response from an acceptor. It will react corresponding to
-	// the message and make updates to the current state.
-	ReceivePrepareResponse()
+	Interpret_message([]byte) error
 
-	// This function is used by a leader upon receiving an accept
-	// response from an acceptor. It will react corresponding to
-	// the message and make updates to the current state.
-	ReceiveAcceptResponse()
+	CreatePrepareMsg() ([]byte, error)
 
-	// This function is used by an acceptor to react to a prepare
-	// message. It will react corresponding to the current state.
-	ReceivePropose()
-
-	// This function is used by an acceptor to react to an accept
-	// message. It will react corresponding to the current state.
-	ReceiveAccept()
+	CreateAcceptMsg(string) ([]byte, error)
 }
